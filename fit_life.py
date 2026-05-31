29fe1ed97769e7283b3752cf955e1fed3ee756bd
qvazic050-oss <qvazic050@gmail.com>
2026-05-31 22:03:10 +0500

    Испровлено

--- fit_life.py
+++ fit_life.py
@@ -1,39 +1,33 @@
 # Проект FitLife - MVP версия 1.0
+MAX_AGE = 95
+MIN_AGE = 18
+MAX_WEIGHT = 300
+MIN_WEIGHT = 30
+MAX_HEIGHT = 2.5
+MIN_HEIGHT = 1.0
 user_name = input("Как вас зовут? ")
-user_age = int(input("Сколько вам лет? "))
 while True:
-    if user_age < 18:
-        print("Ведите корректный возраст")
-        user_age = int(input("Пожалуйста, введите ваш возраст: "))
-    if user_age > 95:
-        print("Ведите корректный возраст")
-        user_age = int(input("Пожалуйста, введите ваш возраст: "))
-    else:
-        break
-user_weight = float(input("Сколько вы весите? ").replace(",", "."))
+    try:
+        user_age = int(input("Сколько вам лет? "))
+        if MIN_AGE <= user_age <= MAX_AGE:
+            break
+    except ValueError:
+        print("Пожалуйста, введите Ваш возраст числом (от 18 до 95 лет).")
 while True:
-    if user_weight < 30:
-        print("Ведите корректный вес")
-        user_weight = float(input("Пожалуйста, введите ваш вес: "))
-    if user_weight > 150:
-        print("Ведите корректный вес")
-        user_weight = float(input("Пожалуйста, введите ваш вес: "))
-    else:
-        break
-user_height = float(input("Какой у вас рост (в метрах)? ").replace(",", "."))
+    try:
+        user_weight = float(input("Сколько вы весите? "))
+        if MIN_WEIGHT <= user_weight <= MAX_WEIGHT:
+            break
+    except ValueError:
+        print("Пожалуйста, введите корректный вес числом (от 30 до 300 кг).")
 while True:
-    if user_height < 1.0:
-        print("Ведите корректный рост")
-        user_height = float(input("Пожалуйста, введите ваш рост (в метрах): "))
-    if user_height > 2.5:
-        print("Ведите корректный рост")
-        user_height = float(input("Пожалуйста, введите ваш рост (в метрах): "))
-    else:
-        break
-bmi = round(user_weight / (user_height ** 2), 1)
-water_ml = round(user_weight * 30)
-water_l = water_ml / 1000
-print(f"Отчет для пользователя: {user_name} ({user_age} лет)")
-print(f"Ваш ИМТ: {bmi}")
-print(f"Рекомендуемая норма воды в день: {water_l} л")
-print("Расчет окончен. Будьте здоровы!")
+    try:
+        user_height = float(input("Какой у вас рост (в метрах)? ").replace(",", "."))
+        if MIN_HEIGHT <= user_height <= MAX_HEIGHT:
+            break
+    except ValueError:
+        print("Пожалуйста, введите корректный рост числом (от 1.0 до 2.5 м).")
+bmi = round(user_weight / (user_height ** 2))
+water_liters= round(user_weight * 30/1000)
+print(f"{user_name}, ваш ИМТ: {bmi:}")
+print(f"Норма воды: {water_liters:} литров в день.") 
